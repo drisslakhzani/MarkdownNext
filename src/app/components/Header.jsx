3b4b9593563d832f226f7f7055b422467516c59e
drisslakhzani <driss.lakhzani.lkz@gmail.com>
2024-07-01 15:47:01 +0100

    fix some bugs

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <Link className="w-full md:w-[75%] lg:w-[57%]  font-medium text-3xl text-white text-center bg-slate-800 p-8 my-6 rounded-md"href={'/'}>
             <div>
-                <h1 className="text-center ">Driss's Blog 💻</h1>
+                <h1 className="text-center ">Driss&apos;s Blog 💻</h1>
             </div>
         </Link>
 
